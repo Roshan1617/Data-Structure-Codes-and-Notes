0ce8e958c0392dc30d05fa8d3479ef069e46bba7
Roshan Singh <roshan.16n@gmail.com>
2023-08-21 13:54:10 +0530

    print all prime number

--- prime_number.java
+++ prime_number.java
@@ -5,15 +5,23 @@ public class prime_number {
 
     static boolean checkPrime(int n) {
         int m = (int)Math.pow((double)n, 0.5);
-        for(int i=2;i<m;i++) {
+        for(int i=2;i<=m;i++) {
             if(n%i == 0) return false;
         } 
         return true;
     }
 
-    public static void main(String[] args) {
-        int num = 28;
+    static void printPrime(int n) {
+        for(int i=2;i<n;i++) {
+            if(checkPrime(i)) {
+                System.out.print(i+" ");
+            }
+        }
+    }
 
-        System.out.println(checkPrime(num));
+    public static void main(String[] args) {
+        int num = 30;
+        System.out.println();
+        printPrime(num);
     }
 }
